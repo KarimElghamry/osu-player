94a4896b97b31a9b73b69f278d86349f0c87d1d8
KarimElghamry <Karimelghamry@gmail.com>
2020-02-10 14:19:36 +0200

    logo hover effect

--- src/components/dancing_logo/DancingLogo.js
+++ src/components/dancing_logo/DancingLogo.js
@@ -4,7 +4,13 @@ import './DancingLogo.css';
 class DancingLogo extends React.Component {
   render () {
     return (
-      <div className="whiteCircle">
+      <div
+        className="whiteCircle"
+        onMouseEnter={() =>
+          document.body.style.setProperty ('--global--width', '600px')}
+        onMouseLeave={() =>
+          document.body.style.setProperty ('--global--width', '500px')}
+      >
         <div className="pinkCircle">osu!</div>
       </div>
     );
