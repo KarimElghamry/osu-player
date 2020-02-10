a099ba3422ff2a306a448472f05a15de3c799514
KarimElghamry <Karimelghamry@gmail.com>
2020-02-10 14:38:48 +0200

    placeholder dancing effect function

--- src/components/dancing_logo/DancingLogo.js
+++ src/components/dancing_logo/DancingLogo.js
@@ -2,14 +2,39 @@ import React from 'react';
 import './DancingLogo.css';
 
 class DancingLogo extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      expanded: false,
+      pauseAnimation: false,
+    };
+  }
+
+  componentDidMount () {
+    this.dancingEffect ();
+  }
+
+  dancingEffect () {
+    setInterval (() => {
+      if (this.state.pauseAnimation) return;
+      this.setState ({expanded: !this.state.expanded});
+      const size = this.state.expanded ? '600px' : '500px';
+      document.body.style.setProperty ('--global--width', size);
+    }, 350);
+  }
+
   render () {
     return (
       <div
         className="whiteCircle"
-        onMouseEnter={() =>
-          document.body.style.setProperty ('--global--width', '600px')}
-        onMouseLeave={() =>
-          document.body.style.setProperty ('--global--width', '500px')}
+        onMouseEnter={() => {
+          this.setState ({pauseAnimation: true, expanded: true});
+          document.body.style.setProperty ('--global--width', '600px');
+        }}
+        onMouseLeave={() => {
+          this.setState ({pauseAnimation: false, expanded: false});
+          document.body.style.setProperty ('--global--width', '500px');
+        }}
       >
         <div className="pinkCircle">osu!</div>
       </div>
