b3d27131b97c499bc868ef03fee89b09632e3128
KarimElghamry <Karimelghamry@gmail.com>
2020-02-10 16:56:18 +0200

    Updates

---
+++ precache-manifest.51da23ca6d2df722d7830f02dc7001eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7657cc6d6b4439c049c1e112e916279f",
+    "url": "/osu-player/index.html"
+  },
+  {
+    "revision": "14a03cdae6b9cdcb4ac1",
+    "url": "/osu-player/static/css/main.ba02b3b0.chunk.css"
+  },
+  {
+    "revision": "768268404fd8a03ec4a1",
+    "url": "/osu-player/static/js/2.8c9444c5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/osu-player/static/js/2.8c9444c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a03cdae6b9cdcb4ac1",
+    "url": "/osu-player/static/js/main.945d0483.chunk.js"
+  },
+  {
+    "revision": "d9ec0f5b7f6663fbcd42",
+    "url": "/osu-player/static/js/runtime-main.7a9539e6.js"
+  },
+  {
+    "revision": "ec478fc73747085c0b7028c642d992b1",
+    "url": "/osu-player/static/media/Aller.ec478fc7.ttf"
+  }
+]);
